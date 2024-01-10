88508ad7c886866c46c0ae0f9347edb86e1c305e
jparkkil <jasper.parkkila@cern.ch>
2024-01-10 01:21:16 +0100

    CF 2-prong filter work

--- PWGCF/TableProducer/filter2Prong.cxx
+++ PWGCF/TableProducer/filter2Prong.cxx
@@ -37,27 +37,41 @@ using namespace o2::math_utils::detail;
 #define O2_DEFINE_CONFIGURABLE(NAME, TYPE, DEFAULT, HELP) Configurable<TYPE> NAME{#NAME, DEFAULT, HELP};
 
 struct FilterCF2Prong{
-  O2_DEFINE_CONFIGURABLE(cfgVerbosity, int, 1, "Verbosity level (0 = major, 1 = per collision)")
-
-  using HFCandidates = soa::Join<aod::HfCand2Prong, aod::HfSelD0>;
-  void processData(soa::Join<aod::Collisions, aod::CFCollRefs>::iterator const& collision, aod::BCsWithTimestamps const&, soa::Join<aod::Tracks, aod::TrackSelection, aod::CFTrackRefs> const& tracks, HFCandidates const& candidates){
-	if (cfgVerbosity > 0) {
-      LOGF(info, "processData2Prong: Candidates for collision: %u", candidates.size());
-    }
-  }
-  int prongCFId[2] = {-1,-1};
-  for(auto &c : candidates)
-  	if(c.prong0Id() == tracks.index()){
-		prongCFId[0] = tracks.cfTrack();
-		break;
+	O2_DEFINE_CONFIGURABLE(cfgVerbosity, int, 1, "Verbosity level (0 = major, 1 = per collision)")
+	using HFCandidates = soa::Join<aod::HfCand2Prong, aod::HfSelD0>;
+	void processData(soa::Join<aod::Collisions, aod::CFCollRefs>::iterator const& collision, aod::BCsWithTimestamps const&, soa::Join<aod::Tracks, aod::TrackSelection, aod::CFTrackRefs> const& tracks, HFCandidates const& candidates){
+		if (cfgVerbosity > 0) {
+			LOGF(info, "processData2Prong: Candidates for collision: %u", candidates.size());
+		}
+		int prongCFId[2] = {-1,-1};
+		for(auto &track : tracks){
+			for(auto &c : candidates)
+				if(c.prong0Id() == track.index()){
+					//prongCFId[0] = track.cfTrack.index();
+					//track.cfTrack().index();
+					//track.cfTrackId();
+					//track.cfTrack_as<aod::CFTrackRefs>().index();
+					prongCFId[0] = track.cfTrack_as<aod::CFTrackRefs>().cfTrackId();
+					//track.template cfTrack_as<aod::CFTrackRefs>().cfTrackId();
+					//track.cfTrack().index();
+					break;
+				}
+			for(auto &c : candidates)
+				if(c.prong1Id() == track.index()){
+					prongCFId[1] = track.cfTrack_as<aod::CFTrackRefs>().cfTrackId();
+					break;
+				}
+		}
+		for (auto& c : candidates) {
+			if ((c.hfflag() & 1 << aod::hf_cand_2prong::DecayType::D0ToPiK) == 0) // TODO <--- make configurable
+				continue;
+			//const auto& m0 = prongTrackMap[0].find(c.prong0Id());
+			//const auto& m1 = prongTrackMap[1].find(c.prong1Id());
+			output2ProngTracks(0, //<-- TODO output2ProngColls.lastIndex(),
+				 prongCFId[0], prongCfId[1], c.pt(), c.eta(),
 	}
-  for(auto &c : candidates)
-  	if(c.prong1Id() == tracks.index()){
-		prongCFId[1] = tracks.cfTrack();
-		break;
-	}
-  PROCESS_SWITCH(FilterCF2Prong, processData, "Process data", true);
-}
+	PROCESS_SWITCH(FilterCF2Prong, processData, "Process data", true);
+}; //struct
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
